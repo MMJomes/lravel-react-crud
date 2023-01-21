93f943655cb81acbeeaff5514543e0a74ffdb7a6
unknown <HeinHtet>
2023-01-21 23:40:34 +0630

    crud

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
+
+
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data= Category::select('id','title','description','image')->get();
+        return $data;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image'=>'required|image'
+        ]);
+        try {
+            $imageName = Str::random().'.'.$request->image->getClientOriginalExtension();
+            Storage::disk('public')->putFileAs('product/image', $request->image,$imageName);
+            Category::create($request->post()+['image'=>$imageName]);
+
+            return response()->json([
+                'message'=>'Product Created Successfully!!'
+            ]);
+
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return response()->json([
+            'product'=>$category
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $product)
+    {
+
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image'=>'nullable'
+        ]);
+
+        try{
+
+            $product->fill($request->post())->update();
+
+            if($request->hasFile('image')){
+
+                // remove old image
+                if($product->image){
+                    $exists = Storage::disk('public')->exists("product/image/{$product->image}");
+                    if($exists){
+                        Storage::disk('public')->delete("product/image/{$product->image}");
+                    }
+                }
+
+                $imageName = Str::random().'.'.$request->image->getClientOriginalExtension();
+                Storage::disk('public')->putFileAs('product/image', $request->image,$imageName);
+                $product->image = $imageName;
+                $product->save();
+            }
+
+            return response()->json([
+                'message'=>'Product Updated Successfully!!'
+            ]);
+
+        }catch(\Exception $e){
+            return response()->json([
+                'message'=>'Something goes wrong while updating a product!!'
+            ],500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $product)
+    {
+        try {
+
+            if($product->image){
+                $exists = Storage::disk('public')->exists("product/image/{$product->image}");
+                if($exists){
+                    Storage::disk('public')->delete("product/image/{$product->image}");
+                }
+            }
+            $product->delete();
+            return response()->json([
+                'message'=>'Product Deleted Successfully!!'
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message'=>'Something goes wrong while deleting a product!!'
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+|https://techvblogs.com/blog/build-crud-app-with-laravel-and-reactjs
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products',CategoryController::class);
