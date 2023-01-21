b1ac251617a5aad86205cf2bcb346b8982fdf960
unknown <HeinHtet>
2023-01-21 23:58:27 +0630

    add_linl

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\CategoryController;
 | Web Routes
 |--------------------------------------------------------------------------
 |https://techvblogs.com/blog/build-crud-app-with-laravel-and-reactjs
+https://blog.devgenius.io/a-blogs-posts-app-with-laravel-react-crud-c714c6c5944f
+https://morioh.com/p/bd404646b056
+https://www.itsolutionstuff.com/post/laravel-react-js-crud-application-tutorialexample.html
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
